b8ce4d367c2507ddece36b7dfe0a64203260de4c
CccrizzZ <ccccrizzzz@gmail.com>
2018-12-04 11:51:25 -0500

    ##

---
+++ Semester1/1007/M3/M3/Game.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <SDL.h>
+class Game {
+private:
+	bool m_bRunning;
+	SDL_Window* m_pWindow;
+	SDL_Renderer* m_pRenderer;
+public:
+	Game() {}	// Default constructor
+	~Game() {} // Default destructor
+
+	bool init(
+		const char* title,
+		int xpos,
+		int ypos,
+		int width,
+		int height,
+		int flags
+	);
+
+	bool running();
+	void update();
+	void handleEvents();
+	void render();
+	void clean();
+};
